73782d4daad028679d33b38af731dcfa205c8ce2
Drewbi <drewalexander986@gmail.com>
2022-07-25 00:05:22 +0800

    implemented some good stuff

--- dist/index.js
+++ dist/index.js
@@ -9049,6 +9049,108 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 9926:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const github = __nccwpck_require__(5438);
+
+let octokit;
+
+const getOctokit = (token) => {
+    if (octokit) return octokit
+    if(!token) throw new Error('Octokit not instantiated and no token provided')
+    return octokit = github.getOctokit(token);
+}
+
+module.exports = { getOctokit }
+
+
+/***/ }),
+
+/***/ 1252:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const { getOctokit } = __nccwpck_require__(9926)
+
+const getStats = async () => {
+    const octokit = getOctokit()
+    const { data } = await octokit.rest.activity.listPublicEvents();
+    return data;
+}
+
+const MAX_LINES = 20
+const MAX_LENGTH = 63
+
+const capitalize = str => str.slice(0, 1).toUpperCase() + str.slice(1)
+const truncate = str =>
+  str.length <= MAX_LENGTH ? str : str.slice(0, MAX_LENGTH - 3) + '...'
+
+const serializers = {
+    WatchEvent: item => {
+        return `🌟 Starred repo: ${item.repo.name}`
+    },
+    IssueCommentEvent: item => {
+        return `🗣 Commented on #${item.payload.issue.number} in ${item.repo.name}`
+    },
+    CreateEvent: item => {
+        if(item.ref_type == 'repository')
+        return `🆕 Created new repo: ${item.repo.name}`
+        else
+        return `⑂ Created branch ${item.payload.ref} in repo: ${item.repo.name}`
+    },
+    ForkEvent: item => {
+        return `ᛘ Forked repo: ${item.repo.name}`
+    },
+    IssuesEvent: item => {
+        return `❗️ ${capitalize(item.payload.action)} issue #${
+        item.payload.issue.number
+        } in ${item.repo.name}`
+    },
+    PullRequestEvent: item => {
+        const emoji = item.payload.action === 'opened' ? '🆒' : '❌'
+        const line = item.payload.pull_request.merged
+        ? '🎉 Merged'
+        : `${emoji} ${capitalize(item.payload.action)}`
+        return `${line} PR #${item.payload.pull_request.number} in ${
+        item.repo.name
+        }`
+    }
+}
+
+const getCommitText = (events) => {
+    const commits = events.filter(event => {
+        const eventDate = new Date(event.created_at)
+        // Return true if event is less than 1 week old
+        return Date.now() - eventDate.getTime() < 7 * 24 * 60 * 60 * 1000
+    })
+    .filter(event => event.type === 'PushEvent')
+
+    const commitCounter = (total, commit) => total + commit.payload.size
+    const commitNum = commits.reduce(commitCounter, 0)
+
+    return `💻 ${ commitNum === 0 ? 'No' : 'Pushed'} ${ commitNum } commit${commitNum > 1 ? 's' : ''} in the last week`
+}
+
+const getEventText = (events) => {
+    return events
+      // Filter out any boring activity
+      .filter(event => serializers[event.type])
+      // We only have four lines to work with for other info
+      .slice(0, MAX_LINES - 1)
+      // Call the serializer to construct a string
+      .map(item => serializers[item.type](item))
+      // Truncate if necessary
+      .map(truncate)
+}
+
+const getCombinedText = (events) => {
+    return [getCommitText(events), ...getEventText(events)].join('\n')
+}
+
+module.exports = { getStats, getCombinedText }
+
+/***/ }),
+
 /***/ 2877:
 /***/ ((module) => {
 
@@ -9218,31 +9320,15 @@ module.exports = require("zlib");
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const github = __nccwpck_require__(5438);
 const core = __nccwpck_require__(2186);
+const { getOctokit } = __nccwpck_require__(9926)
+const { getStats } = __nccwpck_require__(1252)
 
 async function run() {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const authToken = core.getInput('GITHUB_TOKEN');
-    console.log(authToken)
-    const octokit = github.getOctokit(authToken)
-
-    // You can also pass in additional options as a second parameter to getOctokit
-    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
-    const { data: pullRequest } = await octokit.rest.pulls.get({
-        owner: 'octokit',
-        repo: 'rest.js',
-        pull_number: 123,
-        mediaType: {
-          format: 'diff'
-        }
-    });
-
-    return pullRequest;
+  const authToken = core.getInput('GITHUB_TOKEN');
+  getOctokit(authToken)
+  const results = await getStats()
+  return results
 }
 
 run();
